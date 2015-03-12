ec0c41982ac75a178b1c24e112c80ae183b6a778
John Franti & Reid Baldwin <student@epicodus.com>
2015-03-12 16:10:34 -0700

    Reach stopping point.

--- numberwords/src/NumberWord.php
+++ numberwords/src/NumberWord.php
@@ -1,35 +1,59 @@
 <?php
 
     class NumberWord
-    {
-        function wordify($input)
         {
-            $digitcount = count($input);
-            if ($digitcount = 1) {
-                switch($input) {
-                    case 0:
-                        return "zero";
-                    case 1:
-                        return "one";
-                    case 2:
-                        return "two";
-                    case 3:
-                        return "three";
-                    case 4:
-                        return "four";
-                    case 5:
-                        return "five";
-                    case 6:
-                        return "six";
-                    case 7:
-                        return "seven";
-                    case 8:
-                        return "eight";
-                    case 9:
-                        return "nine";
-                }
-            }
-        }
+            function wordify($input)
+                {
+                    $array = array();
+                    $called_number = array_push($array, $input);
+                    $wide_array = str_split($called_number);
+                    $digitcount = count($wide_array);
+                        if ($digitcount == 1) {
+                            switch($input) {
+                                case 0:
+                                    return "zero";
+                                case 1:
+                                    return "one";
+                                case 2:
+                                    return "two";
+                                case 3:
+                                    return "three";
+                                case 4:
+                                    return "four";
+                                case 5:
+                                    return "five";
+                                case 6:
+                                    return "six";
+                                case 7:
+                                    return "seven";
+                                case 8:
+                                    return "eight";
+                                case 9:
+                                    return "nine";
+                                }
+                            }
+                        elseif ($digitcount == 2) {
+                            switch($input) {
+                                case 10:
+                                    return "ten";
+                                case 20:
+                                    return "twenty";
+                                case 30:
+                                    return "thirty";
+                                case 40:
+                                    return "forty";
+                                case 50:
+                                    return "fifty";
+                                case 60:
+                                    return "sixty";
+                                case 70:
+                                    return "seventy";
+                                case 80:
+                                    return "eighty";
+                                case 90:
+                                    return "ninety";
+                                }
+                            }
+                        }
     }
-
 ?>
--- numberwords/tests/NumberWordTest.php
+++ numberwords/tests/NumberWordTest.php
@@ -3,17 +3,17 @@
 require_once "src/NumberWord.php";
 class NumberWordTest extends PHPUnit_Framework_TestCase
 {
-    function test_wordify_one()
-    {
-    //Arrange
+    function test_wordify_one() {
     $test_NumberWord = new NumberWord;
-    $input = "1";
-
-    //Act
+    $input = 2;
     $result = $test_NumberWord->wordify($input);
-
-    //Assert
-    $this->assertEquals("one", $result);
+    $this->assertEquals("two", $result);
+    }
+    function test_wordify_two() {
+    $test_NumberWord = new NumberWord;
+    $input = 10;
+    $result = $test_NumberWord->wordify($input);
+    $this->assertEquals("ten", $result);
     }
 }
 
