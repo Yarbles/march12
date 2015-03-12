f0b3784bd5fc282f6e72e651de4fc04228bdaa53
John Franti & Reid Baldwin <student@epicodus.com>
2015-03-12 10:54:54 -0700

    Complete function.

--- scrabble/tests/ScrabbleTest.php
+++ scrabble/tests/ScrabbleTest.php
@@ -28,6 +28,30 @@
         //Assert
         $this->assertEquals(2, $result);
         }
+        function test_score_ab()
+        {
+        //Arrange
+        $test_Scrabble = new Scrabble;
+        $input = "ad";
+
+        //Act
+        $result = $test_Scrabble->score($input);
+
+        //Assert
+        $this->assertEquals(3, $result);
+        }
+        function test_score_poop()
+        {
+        //Arrange
+        $test_Scrabble = new Scrabble;
+        $input = "poop";
+
+        //Act
+        $result = $test_Scrabble->score($input);
+
+        //Assert
+        $this->assertEquals(8, $result);
+        }
 
 
 
