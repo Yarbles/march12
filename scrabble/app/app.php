21d437100ae935f9a171e3337b716a922f36dd9c
John Franti & Reid Baldwin <student@epicodus.com>
2015-03-12 11:22:20 -0700

    add website.

--- scrabble/app/app.php
+++ scrabble/app/app.php
@@ -4,8 +4,20 @@
 
     $app = new Silex\Application();
 
+    $app['debug']=true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
     $app->get("/", function() use ($app) {
-        return "Home";
+        return $app['twig']->render("form.twig");
+    });
+
+    $app->get("/results", function() use ($app) {
+        $entry = new Scrabble;
+        $totalscore = $entry->score($_GET["input"]);
+        return $app['twig']->render("results.twig", array('points' => $totalscore));
     });
 
 
